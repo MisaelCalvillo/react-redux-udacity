26c8f7357ba9561d4a4d50e8ccd1165baa0fe71c
Misael Calvillo Mancilla <calvillomisa@hotmail.com>
2019-03-02 01:24:00 -0600

    redux-store: Create store dispatch

--- redux-store/index.js
+++ redux-store/index.js
@@ -1,4 +1,39 @@
-// Reduce todos
+/* Library code */
+function createStore (reducer) {
+  // The store should have four parts
+  // 1. The state
+  // 2. Get the state.
+  // 3. Listen to changes on the state.
+  // 4. Update the state
+  
+  let state
+  let listeners = []
+  
+  const getState = () => state
+  
+  const subscribe = (listener) => {
+    listeners.push(listener)
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+  
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+  
+  return {
+    getState,
+    subscribe,
+    dispatch,
+  }
+}
+
+
+/* App code */
+
+// Reducer todos
 function todos (state =  [], action) {
   if (action.type === 'ADD_TODO') {
     return state.concat([action.todo])
@@ -7,32 +42,7 @@ function todos (state =  [], action) {
   return state
 }
 
-function createStore () {
-  // The store should have four parts
-  // 1. The state
-  // 2. Get the state.
-  // 3. Listen to changes on the state.
-  // 4. Update the state
-
-  let state
-  let listeners = []
-
-  const getState = () => state
-
-  const subscribe = (listener) => {
-    listeners.push(listener)
-    return () => {
-      listeners = listeners.filter((l) => l !== listener)
-    }
-  }
-
-  return {
-    getState,
-    subscribe
-  }
-}
-
-const store = createStore()
+const store = createStore(todos)
 
 store.subscribe(() => {
   console.log('The new state is: ', store.getState())
